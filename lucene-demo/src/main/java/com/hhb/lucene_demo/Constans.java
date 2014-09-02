a012d3575dc1e267396bf278bba05ac403150652
1207263 <hip0216@163.com>
2014-09-02 14:40:32 +0800

    lucene4.6-demo

---
+++ lucene-demo/src/main/java/com/hhb/lucene_demo/Constans.java
@@ -0,0 +1,15 @@
+package com.hhb.lucene_demo;
+
+/**
+ * @ClassName: Constans
+ * @Description: TODO(常量)
+ * @author hip0216@163.com
+ * @date 2014年8月27日 下午2:11:30
+ * 
+ */
+public class Constans {
+
+	public final static String INDEX_FILE_PATH = "E:\\mywork\\Java\\lucene\\test"; // 索引的文件的存放路径
+	public final static String INDEX_STORE_PATH = "E:\\mywork\\Java\\lucene\\index"; // 索引的存放位置
+
+}
